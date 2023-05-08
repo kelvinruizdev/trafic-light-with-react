7936cb2e0e6fd70ead8477f831389c0ea3542699
Kelvin Ruiz <kelvinruiz.developer@gmail.com>
2023-05-07 21:17:22 -0800

    logrado alternar luces con boton, falta finalizar color purple con boton

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const TrafficLight = () => {
+
+	//crear variable useState
+	const [color, setColor] = useState("green");
+
+	function alternateColor(){
+
+		switch (color) {
+			case "red":
+				setColor("yellow");	
+				break;
+			case "yellow":
+				setColor("green");	
+				break;
+			case "green":
+				setColor("red");
+				break;
+			default:
+				setColor("purple");
+				break;	
+		}
+	}
+
+	function addColor(){
+		let box = document.getElementsByClassName("box");
+		let light = document.createElement('div');
+		let buttonDisable = document.getElementsByTagName("button")[1];
+		buttonDisable.disabled = true;
+		light.className = "light lightPurple";
+		light.addEventListener("click", function(){
+			light.className = "light lightPurple purple";
+		})
+		box[0].appendChild(light);	
+	}
+
+	/*useEffect(()=>(
+
+	),[])*/
+	
+	//dibujar semaforo
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-flex"> 
+			<div className="base">  {/*soporte del semaforo*/}
+
+			</div>
+			<div className="box">	{/*caja de luces*/}
+				<div	onClick={()=>setColor("red")} 
+						className={"light lightRed " + ((color == "red")? "red": "")}>  
+				
+				</div>
+				<div 	onClick={()=>setColor("yellow")}
+						className={"light lightYellow " + ((color == "yellow")? "yellow": "")}>
+				
+				</div>
+				<div 	onClick={()=>setColor("green")}
+						className={"light lightGreen " + ((color == "green")? "green": "")}>
+				
+				</div>
+			</div>
+			
+			<div className="d-flex justify-content-center mt-5">
+				<button onClick={alternateColor} className="btn btn-primary m-1">
+					Alternate color
+				</button>
+
+				<button onClick={addColor} className="btn btn-primary m-1">
+					Add color purple
+				</button>
+			</div>
+			
 		</div>
 	);
 };
 
-export default Home;
+export default TrafficLight;
+
+
+
+
+
